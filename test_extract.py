57b4f26bb18357496b8d1edc2f1792d7cf5dfdcf
pg-90 <geresipali90@gmail.com>
2025-03-10 10:29:37 +0100

    update new symbols

--- test_extract.py
+++ test_extract.py
@@ -1,14 +1,20 @@
 from datetime import datetime, timedelta
 import yfinance as yf
-
+import json
 from extractor import Extractor
 
 start_date = (datetime.today() - timedelta(days=151)).strftime("%Y-%m-%d")
 
-extractor = Extractor(
-    tickers=["GMVM.DE"],
-    start_date=str(start_date),
-    end_date=datetime.now().strftime("%Y-%m-%d"),
-    target_folder="data/tickers",
-)
-extractor.extract_data()
+with open("data/lightyear_yfinance_etf_data.json", "r") as f:
+    data = json.load(f)
+
+for d in data.values():
+    extractor = Extractor(
+        tickers=[d],
+        start_date=str(start_date),
+        end_date=datetime.now().strftime("%Y-%m-%d"),
+        target_folder="data/tickers",
+    )
+    result = extractor.extract_data()
+    print(result)
+## 'DBXN.DE' , 'DFND.PA', wbit.du, weth.du
